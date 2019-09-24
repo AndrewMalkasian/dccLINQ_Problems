296a7577a3075309b2646251c0a80aec3a1879a3
Misty Malkasian <andrewandmisty@gmail.com>
2019-09-24 09:16:18 -0500

    commented the instructions in program. Made StringTheory.cs class with public delegate list DoThingToStringList(List<string> parameter) and began first problem ReturnTHSubStrings method

---
+++ StringTheory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Problems
+{
+    public class StringTheory
+    {
+        public delegate List<string> DoThingToStringList(List<string> strings);
+
+        public List<string> ReturnTHSubStrings(List<string> strings)
+        {
+
+        }
+
+    }
+}
